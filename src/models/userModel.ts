b225bf71c153536c7395421d515b45ab8dafeda0
chidera nwokoye <chideranwokoye555@gmail.com>
2024-09-05 17:38:54 +0100

    first commit

---
+++ src/models/userModel.ts
@@ -0,0 +1,56 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import bcrypt from "bcrypt";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ unique: true })
+  username?: string;
+
+  @Column({ unique: true })
+  email?: string;
+
+  @Column()
+  firstName?: string;
+
+  @Column()
+  lastName?: string;
+
+  @Column()
+  age?: string;
+
+  @Column()
+  password?: string;
+  // This method is used to compare the entered password with the hashed password
+  async matchPassword(enteredPassword: string): Promise<boolean> {
+    return await bcrypt.compare(enteredPassword, this.password!);
+  }
+  // This method is used to hash the password before saving it to the database
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (this.password) {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    }
+  }
+
+  @Column({ default: false })
+  verified?: boolean;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}
